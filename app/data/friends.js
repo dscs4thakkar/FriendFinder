1314a3555ab5328bccd3ab33ca960c5812c68529
dscs4thakkar <dscs4thakkar@yahoo.com>
2019-10-15 22:05:37 -0500

    server.js and html pages complete

---
+++ app/data/friends.js
@@ -0,0 +1,131 @@
+var friends = [{
+    "name": "James Bond",
+    "photo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.forbes.com%2Fsites%2Fjeffewing%2F2018%2F09%2F26%2Fstudied-not-stirred-bond-in-numbers-analyzes-everything-james-bond%2F&psig=AOvVaw0YtrEYyXGxv2lrLmROP0gM&ust=1571277543063000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJCntZDXn-UCFQAAAAAdAAAAABAE",
+    "scores": [
+        5,
+        2,
+        1,
+        4,
+        2,
+        2,
+        4,
+        2,
+        2,
+        3
+    ]
+},
+{
+    "name": "Iron Man",
+    "photo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cinemablend.com%2Fnews%2F2471343%2Fapparently-robert-downey-jr-didnt-want-to-do-tony-starks-last-big-line-in-avengers-endgame&psig=AOvVaw24AmwWGM-qXGAEWFiOd1KK&ust=1571277750791000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLiJ0fPXn-UCFQAAAAAdAAAAABAE",
+    "scores": [
+        5,
+        4,
+        3,
+        3,
+        5,
+        2,
+        1,
+        1,
+        3,
+        3
+    ]
+},
+{
+    "name": "Batman",
+    "photo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.news.com.au%2Fentertainment%2Fmovies%2Fben-affleck-and-henry-cavill-reportedly-out-as-batman-and-superman%2Fnews-story%2F4a5c73b0854d133cda41f4065c55da72&psig=AOvVaw0du8kfqz1MlxxmJvcBMh4Y&ust=1571277854731000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCAlKXYn-UCFQAAAAAdAAAAABAF",
+    "scores": [
+        1,
+        4,
+        2,
+        2,
+        5,
+        1,
+        3,
+        4,
+        3,
+        1
+    ]
+},
+{
+    "name": "Gandalf",
+    "photo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Flotr.fandom.com%2Fwiki%2FGandalf&psig=AOvVaw1nzK0sTMi3zbND9JfLJUjA&ust=1571277958614000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMCa09bYn-UCFQAAAAAdAAAAABAE",
+    "scores": [
+        3,
+        5,
+        3,
+        3,
+        3,
+        4,
+        4,
+        5,
+        1,
+        4
+    ]
+},
+{
+    "name": "Hermione Granger",
+    "photo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fharrypotter.fandom.com%2Fwiki%2FHermione_Granger&psig=AOvVaw0kREz84n3G1o3mYqTKT2VS&ust=1571278036740000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLjk5fvYn-UCFQAAAAAdAAAAABAE",
+    "scores": [
+        1,
+        3,
+        2,
+        3,
+        5,
+        5,
+        1,
+        3,
+        4,
+        1
+    ]
+},
+{
+    "name": "Lord Voldemort",
+    "photo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FLord_Voldemort&psig=AOvVaw2CLNsjBd6Z7NhikeWLBmpZ&ust=1571278143924000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJjelK_Zn-UCFQAAAAAdAAAAABAE",
+    "scores": [
+        5,
+        1,
+        4,
+        1,
+        2,
+        1,
+        3,
+        4,
+        5,
+        4
+    ]
+},
+{
+    "name": "Frodo Baggins",
+    "photo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdwightlongenecker.com%2Fhappy-new-year-frodo-baggins%2F&psig=AOvVaw3-wcxZem8u3WyTDmm6ALqg&ust=1571278217004000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNjH8dHZn-UCFQAAAAAdAAAAABAJ",
+    "scores": [
+        5,
+        3,
+        1,
+        2,
+        2,
+        4,
+        1,
+        5,
+        2,
+        1
+    ]
+},
+{
+    "name": "Katniss Everdeen",
+    "photo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fthehungergames.fandom.com%2Fwiki%2FKatniss_Everdeen&psig=AOvVaw1BbMP0AxNCGRlfd5JPw5bA&ust=1571278313565000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMjq6P_Zn-UCFQAAAAAdAAAAABAE",
+    "scores": [
+        3,
+        1,
+        1,
+        5,
+        2,
+        1,
+        2,
+        5,
+        4,
+        5
+    ]
+}
+];
+
+module.exports = friends;
